50de56d5bf6ecb089cebb36002e6ffa0c0b3f670
matheusaralves <matheus.dearaujo@mercadolivre.com>
2022-10-03 19:15:31 -0300

    Atualizando

--- exercises/src/spring/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercises/src/spring/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,16 +1,20 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import br.com.cod3r.exerciciossb.model.entities.Produto;
+import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 
-    @PostMapping
-    public @ResponseBody Produto novoProduto(@RequestParam String nome) {
-        Produto produto = new Produto(nome);
+    private ProdutoRepository produtoRepository;
 
+    @PostMapping
+    public @ResponseBody Produto novoProduto(@RequestParam String nome, @RequestParam double preco, @RequestParam double desconto) {
+        Produto produto = new Produto(nome, preco, desconto);
+        produtoRepository.save(produto);
         return produto;
     }
 }
--- exercises/src/spring/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/Produto.java
+++ exercises/src/spring/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/Produto.java
@@ -11,12 +11,32 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
+    private double preco;
+    private double desconto;
 
     public Produto() {
     }
 
-    public Produto(String nome) {
-        this.nome = nome;
+    public Produto(String nome, double preco, double desconto) {
+        setNome(nome);
+        setPreco(preco);
+        setDesconto(desconto);
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
     }
 
     public int getId() {
